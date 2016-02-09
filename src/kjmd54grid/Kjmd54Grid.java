a7548e0da34ccb29fd1253a66dce933bf3442d97
Kyle McCarthy <km@kylemccarthy.io>
2016-02-09 13:46:05 -0600

    cleaning up

--- src/kjmd54grid/Kjmd54Grid.java
+++ src/kjmd54grid/Kjmd54Grid.java
@@ -9,7 +9,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 /**
  *
