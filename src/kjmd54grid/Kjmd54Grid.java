1cdd041131f1a18d4826f5cd4143db0d8aee96c5
Kyle McCarthy <km@kylemccarthy.io>
2016-02-08 20:15:59 -0600

    import the requried classes and also change root to GridPane

--- src/kjmd54grid/Kjmd54Grid.java
+++ src/kjmd54grid/Kjmd54Grid.java
@@ -9,9 +9,10 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -21,18 +22,7 @@ public class Kjmd54Grid extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        GridPane root = new GridPane();
         
         Scene scene = new Scene(root, 300, 250);
         
